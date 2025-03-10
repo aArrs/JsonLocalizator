57ade15dbdfececfce680ceae7c2cf291cbda951
Arseniy Yuhnovets <closserd@mail.ru>
2025-03-10 12:41:44 +0300

    build: rebuild project

--- Form1/obj/Release/net8.0-windows/Form1.AssemblyInfo.cs
+++ Form1/obj/Release/net8.0-windows/Form1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Form1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564c1d659214b0e1d55d7d4148c7159fb12eccc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Form1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Form1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
